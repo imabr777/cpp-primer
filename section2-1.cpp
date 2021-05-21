2ef0fe09e6bc1e2c5af0f9920fb28d8940a71886
Abraham Im <abeim10@gmail.com>
2021-05-21 13:58:15 -0700

    Section 2-1 finished

--- section2-1.cpp
+++ section2-1.cpp
@@ -1,5 +1,5 @@
 /*
- * Section 2.1
+ * Section 2.1 Primitive Built-In Types
  */
 
 // Exercise 2.1
@@ -21,3 +21,75 @@
 
 // For the payment, I would use a double, because typical house payments are in the 6 significant figure range (e.g. 1132.56), but I would like to have some flexibility
 // for the some extremely expensive houses, so I would use a double.
+
+// Exercise 2.3
+// 32
+// Max value of integer on the machine - 31 (because 0 is also an integer) (my result is 4294967264)
+
+// 32
+// -32
+
+// 0
+// 0
+
+// Exercise 2.4
+// #include <iostream>
+
+// int main() 
+// {
+// 	unsigned u = 10, u2 = 42;
+// 	std::cout << u2 - u << std::endl;
+// 	std::cout << u - u2 << std::endl;
+// 	int i = 10, i2 = 42;
+// 	std::cout << i2 - i << std::endl;
+// 	std::cout << i - i2 << std::endl;
+// 	std::cout << i - u << std::endl;
+// 	std::cout << u - i << std::endl;
+
+// 	return 0;
+// }
+
+// Exercise 2.5
+// (a)   'a' is a character literal
+//      L'a' is a wide character (wchar_t) literal
+//       "a" is a string literal
+//      L"a" is a wide character (wchar_t) literal
+// (b)    10 is an integer literal, type int
+//       10u is an integer literal, type unsigned int
+//       10L is an integer literal, type long
+//      10uL is an integer literal, type unsigned long
+//       012 is an octal integer for the decimal number 10
+//       0xC is a hexidecimal integer, equal to decimal number 12
+// (c)  3.14 is a floating point literal, type double
+//     3.14f is a floating point literal, type float
+//     3.14L is a floating point literal, type long double
+// (d)    10 is an integer literal, type int
+//       10u is an integer literal, type unsigned int
+//       10. is a floating point literal, type double
+//     10e-2 is a floating point literal, type double
+
+// Exercise 2.6
+// int month = 9, day = 7;
+// int month = 09, day = 07;
+// The first definition uses normal integers, the second one uses octal integers.
+// The second one doesn't compile because 9 is outside the range for base 8 numbers.
+
+// Exercise 2.7
+// (a) "Who goes with F\145rgus? \012"
+//     This is a string literal, and will display as "Who goes with Fergus?" with an LF (line feed) at the end. I believe it's the same as a newline
+// (b) 3.14e1L
+//     This is a floating point literal, type long double, and has the value 31.4
+// (c) 1024f
+//     This is a floating point literal, type double, value 1024.0
+// (d) 3.14L
+//     This is a floating point literal, type long double, value 3.14
+
+// Exercise 2.8
+// #include <iostream>
+
+// int main()
+// {
+// 	std::cout << "\62\115\12" << std::endl;
+// 	std::cout << "\62\11\115\12" << std::endl;
+// 	return 0;
+// }
